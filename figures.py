e0ab1b6235eb47ee288b89e927a5f8881d7cd5b9
ichistov <ichistov@eng.vsofts.com>
2020-01-30 19:22:27 +0300

    added class Trangle

--- figures.py
+++ figures.py
@@ -6,28 +6,34 @@ class GeometryFigure:
     _perimeter = None
     _area = None
 
-    def __init__(self, name, perimeter, area, angles):
+    def __init__(self, name, angles):
         """Class constructor for geometry figure"""
         self.name = name
-        self.perimeter = perimeter
-        self.area = area
         self.angles = angles
-        print('Geometry figure {name} was created!')
+        print(f'Geometry figure {name} with {angles} angles was created!')
 
     def __str__(self):
         return self._name
     
-    def perimeter_calculation(self):
-        return "{name} figure perimeter is equals {perimeter}"
-
-    def area_calculation(self):
-        return "{name} figure area is equals {area}"
+    def get_name_and_angles(self):
+        return f"{self.name} with {self.angles} angles have "
 
 
 
 class Trangle(GeometryFigure):
 
-    def __init__(self, name, perimeter, area):
-        super().__init__(name, perimeter, area, 3)
-        self.name = 'Trangle'
-        self.perimeter
+    def __init__(self, name, a, b, c):
+        super().__init__(name, 3)
+        self.a = a
+        self.b = b
+        self.c = c
+
+
+    def trangle_perimeter_calculation(self):
+        perimeter = self.a + self.b + self.c
+        return self.get_name_and_angles() + 'perimeter = ' + str(perimeter)
+
+
+if __name__ == '__main__':
+    trangle = Trangle(name='Trangle', a=5, b=5, c=10)
+    print(trangle.trangle_perimeter_calculation())
