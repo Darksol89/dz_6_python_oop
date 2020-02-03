115445639e04db73d40170a4b5c258e214d54d2b
ichistov <ichistov@eng.vsofts.com>
2020-02-03 10:49:15 +0300

    Исправлены методы классов. Добавлен метод получения имени и кол-ва углов

--- source/figures.py
+++ source/figures.py
@@ -14,13 +14,19 @@ class GeometryFigure:
         self.name = name
         self.angles = angles
 
+    def get_name_and_angles(self):
+        if self.angles > 0:
+            return f'{self.name} with {self.angles} angles'
+        else:
+            return f'{self.name}'
+
     def add_area(self, other_figure):
         """The sum of the areas different figures"""
         if isinstance(other_figure, GeometryFigure):
-            print(self.area() + other_figure.area())
+            sum_figures = self.area() + other_figure.area()
+            return sum_figures
         else:
-            #raise AttributeError('Function got unknown class')
-            print('Function got unknown class')
+            raise AttributeError('Function got unknown class')
 
 
 class Triangle(GeometryFigure):
@@ -32,18 +38,17 @@ class Triangle(GeometryFigure):
         self.a = a
         self.b = b
 
-
     def perimeter(self):
         """Perimeter calculation"""
         c = math.sqrt(self.a ** 2 + self.b ** 2)
         perimeter = self.a + self.b + c
-        print(f'Perimeter {self.name} with {self.angles} angles is {int(perimeter)}')
+        print(f'Perimeter {self.get_name_and_angles()} is {int(perimeter)}')
         return int(perimeter)
 
     def area(self):
         """Area calculation"""
         area = 1 / 2 * self.a * self.b
-        print(f'Area {self.name} with {self.angles} angles is {int(area)}')
+        print(f'Area {self.get_name_and_angles()} is {int(area)}')
         return int(area)
 
 
@@ -58,13 +63,13 @@ class Square(GeometryFigure):
     def perimeter(self):
         """Perimeter calculation"""
         perimeter = 4 * self.a
-        print(f'Perimeter {self.name} with {self.angles} angles is {int(perimeter)}')
+        print(f'Perimeter {self.get_name_and_angles()} is {int(perimeter)}')
         return int(perimeter)
 
     def area(self):
         """Area calculation"""
         area = self.a ** 2
-        print(f'Area {self.name} with {self.angles} angles is {int(area)}')
+        print(f'Area {self.get_name_and_angles()} is {int(area)}')
         return int(area)
 
 
@@ -82,13 +87,13 @@ class Rectangle(GeometryFigure):
     def perimeter(self):
         """Perimeter calculation"""
         perimeter = 2 * self.a + 2 * self.b
-        print(f'Perimeter {self.name} with {self.angles} angles is {int(perimeter)}')
+        print(f'Perimeter {self.get_name_and_angles()} is {int(perimeter)}')
         return int(perimeter)
 
     def area(self):
         """Area calculation"""
         area = self.a * self.b
-        print(f'Area {self.name} with {self.angles} angles is {int(area)}')
+        print(f'Area {self.get_name_and_angles()} is {int(area)}')
         return int(area)
 
 
@@ -103,13 +108,13 @@ class Circle(GeometryFigure):
     def perimeter(self):
         """Perimeter calculation"""
         perimeter = self.radius * 2 * math.pi
-        print(f'Perimeter {self.name} with {self.angles} angles is {int(perimeter)}')
+        print(f'Perimeter {self.get_name_and_angles()} is {int(perimeter)}')
         return int(perimeter)
 
     def area(self):
         """Area calculation"""
         area = self.radius ** 2 * math.pi
-        print(f'Area {self.name} is {int(area)}')
+        print(f'Area {self.get_name_and_angles()} is {int(area)}')
         return int(area)
 
 
@@ -120,5 +125,7 @@ if __name__ == "__main__":
     circle = Circle(radius=1.5)
     tesd = 10
 
-    #triangle.area()
-    triangle.add_area(triangle)
+    triangle.area()
+    circle.perimeter()
+    circle.area()
+    #triangle.add_area(triangle)
